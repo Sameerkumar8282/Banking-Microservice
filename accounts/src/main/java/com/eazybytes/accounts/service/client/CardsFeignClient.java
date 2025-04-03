4c6cbb2135afd2b88101e784de22e005d923ac66
sameerkumar8282 <sameerkumar1012.sk@gmail.com>
2025-04-03 13:02:34 +0530

    API Gateway with the API Trace Request Response Implemented

--- accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
@@ -5,11 +5,15 @@ import jakarta.validation.constraints.Pattern;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //name should be given same as in application.properties of that application
 @FeignClient("cards")
 public interface CardsFeignClient {
-    @GetMapping("/api/fetch")
-    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("eazybank-correlation-id")
+                                                     String correlationId, @RequestParam String mobileNumber);
+
 }
